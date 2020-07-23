a1a64a0d20c858e3f95b4f543deb3a4df8f1acc3
Elmer Chandia <65907732+chandi17@users.noreply.github.com>
2020-07-22 22:36:41 -0600

    Crear clase conexion

--- ProyectoHotel/ProyectoHotel/Conexion.vb
+++ ProyectoHotel/ProyectoHotel/Conexion.vb
@@ -1,3 +1,5 @@
-﻿Public Class Class1
+﻿Imports System.Data.SqlClient
+Public Class Conexion
+    Public conexion As SqlConnection = New SqlConnection("Data Source = CHANDIA;Initial Catalog=ProyectoHotel;Integrated Security=True")
 
 End Class
